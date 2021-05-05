971ed35d4f5e505c915166a45bd0af0eab771f46
Hala Mohammed <HalaMohammed1245@gmail.com>
2021-05-05 04:02:12 +0200

    Initial Commit

---
+++ OTP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTP
+//
+//  Created by Hala on 05/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
